5485e3ef87f9ca41d57e0ce87cc3949d4ba77604
rakeshcheekatimala <rakeshcheekatimala@gmail.com>
2020-09-05 10:00:47 +0800

    applying style changes for Question Widget

--- first_app/lib/question.dart
+++ first_app/lib/question.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class  Question extends StatelessWidget {
+class Question extends StatelessWidget {
   final String questionText; // to avoid changing this property inside class
   // we are telling dart, the value never change after initialization
 
@@ -8,7 +8,12 @@ class  Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(width:double.infinity, // this takes the full width of the container
+    margin: EdgeInsets.all(10),
+    child:Text(
+      questionText,
+      style: TextStyle(fontSize: 28),
+      textAlign: TextAlign.center,
+    ));
   }
-  
 }
